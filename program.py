ce6a8f30811a938d2a03f4b61b21d89f8edab543
VictorCodes <clausing2901@gmail.com>
2016-10-18 08:10:15 -0400

    Update program.py

--- program.py
+++ program.py
@@ -1,4 +1,15 @@
-##Code for game inventory/item list
-itemNumber = ['Pocket Lint', 'Dagger', 'Potion', 'Ragged Clothes']
+#Code for game inventory/item list
+#Sets a number to each item
+iN = ['Pocket Lint', 'Dagger', 'Potion', 'Ragged Clothes']
+#Sets a sell amount to each item
+iNW = {'iN(0)': 0, 'iN(1)': 12, 'iN(2)': 24, 'iN(3)': 30}
+#Sets a buy amount to each item
+iNB = {'iN(0)': 2, 'iN(1)': 32, 'iN(2)': 35, 'iN(3)': 45}
 money = 500
-inventory = ['']
+inventory = ['iN(1)']
+#Prints what you have in your inventory and how much each item is worth wether buying or selling, and how much money you have
+for key in inventory:
+    print key
+    print "Your money: " money
+    print "Sell: %s" % iNW[key]
+    print "Buy: %s" % iNB[key]
